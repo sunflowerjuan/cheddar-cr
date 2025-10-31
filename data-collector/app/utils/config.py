8a3fcae51f1c407122a6bb9677c3d6799a8cf3dd
suflowerjuan <sebasjuan331@gmail.com>
2025-10-30 22:36:35 -0400

    temp data_dir excluded

--- data-collector/app/utils/config.py
+++ data-collector/app/utils/config.py
@@ -6,4 +6,4 @@ load_dotenv()
 TOKEN = os.getenv("TOKEN")
 BASE_URL = "https://api.clashroyale.com/v1"
 HEADERS = {"Authorization": f"Bearer {TOKEN}"}
-DATA_DIR = os.getenv("DATA_DIR", "/app/data")
+
