7e882fa01635ff7f876bfcc90e20afb763ded827
suflowerjuan <sebasjuan331@gmail.com>
2025-10-31 01:49:00 -0400

    main swagger docs finished

--- data-collector/app/main.py
+++ data-collector/app/main.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flasgger import Swagger
 from routes.cr_router import router_bp
+from utils.config import swagger_config,template
 
 app = Flask(__name__)
-
+swagger = Swagger(app, config=swagger_config, template=template)
 app.register_blueprint(router_bp)
 
 @app.route('/')
@@ -10,4 +12,4 @@ def home():
     return "Data Collector is running."
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
