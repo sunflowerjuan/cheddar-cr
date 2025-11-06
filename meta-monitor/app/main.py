e57f95efdd205f89d676f96b0155eca3d19e4d0d
suflowerjuan <sebasjuan331@gmail.com>
2025-11-06 18:06:30 -0500

    API main file

---
+++ meta-monitor/app/main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flasgger import Swagger
+from routes.monitor_router import router_bp
+from utils.config import swagger_config,template
+
+app = Flask(__name__)
+swagger = Swagger(app, config=swagger_config, template=template)
+app.register_blueprint(router_bp)
+
+@app.route('/')
+def home():
+    return "Data Collector is running."
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
