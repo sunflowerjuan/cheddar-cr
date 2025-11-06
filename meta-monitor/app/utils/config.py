c316452bcdd9e7eeabd44f0cfc09d9940bf270ec
suflowerjuan <sebasjuan331@gmail.com>
2025-11-06 18:05:47 -0500

    added Swagger configuration

--- meta-monitor/app/utils/config.py
+++ meta-monitor/app/utils/config.py
@@ -9,3 +9,28 @@ STATICS_URLS = {
     "camino_trofeos": "https://statsroyale.com/es/top/cards?type=ladder"
 }
 WAIT_TIME = 5 
+
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": "apispec_1",
+            "route": "/apidocs/apispec_1.json", 
+            "rule_filter": lambda rule: True,
+            "model_filter": lambda tag: True,
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/apidocs/",
+}
+
+template = {
+    "swagger": "2.0",
+    "info": {
+        "title": "Meta-Monitor API from Cheddar-CR ",
+        "description": "API para recolección informacion acerca de cambios en el meta de Clash Royale, basado en análisis de blogs oficiales y estadísticas de cartas.",
+        "version": "1.0.0",
+    },
+    "basePath": "/",
+}
