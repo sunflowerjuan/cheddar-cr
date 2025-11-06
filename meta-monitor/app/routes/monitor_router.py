2ff543e5508afd90894cae7c8acbe0ebb20dc6c5
suflowerjuan <sebasjuan331@gmail.com>
2025-11-06 18:06:02 -0500

    fix meta return error in json parsing

--- meta-monitor/app/routes/monitor_router.py
+++ meta-monitor/app/routes/monitor_router.py
@@ -11,7 +11,7 @@ router_bp = Blueprint('router', __name__, url_prefix='/')
 def balance_endpoint():
     """Recolecta datos asociados a los cambios de balance."""
     data = meta_data()                    
-    return jsonify(json.loads(data)), 200  
+    return jsonify(data), 200
 
 @router_bp.route('/stadistics', methods=['GET'])
 @swag_from('docs/stadistics.yaml')
