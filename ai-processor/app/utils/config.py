722486b58d6ff51b29eef8b8794f09913098d12a
suflowerjuan <sebasjuan331@gmail.com>
2025-11-10 14:59:22 -0500

    added rabbitmq variables

--- ai-processor/app/utils/config.py
+++ ai-processor/app/utils/config.py
@@ -6,6 +6,18 @@ load_dotenv()
 LLM_BASE_URL = os.getenv("BASE_URL")
 PLAYER_API_BASE_URL = "http://cr.localhost/collector"
 META_API_BASE_URL = "http://cr.localhost/monitor"
+MCP_SERVER_URL = "http://127.0.0.1:8000"
 
 KEY_PROMPT = os.getenv("KEY_PROMPT")
 
+server_config = {
+    "default": {
+        "url": f"{MCP_SERVER_URL}/sse",
+        "transport": "sse",
+    }
+}
+
+
+RABBITMQ_HOST = "localhost"
+RABBITMQ_USER = os.getenv("RABBITMQ_DEFAULT_USER")
+RABBITMQ_PASS = os.getenv("RABBITMQ_DEFAULT_PASS")
